ee26f941a14638f69781325496b688cd514d88de
Tommy <tomekkoczar@gmail.com>
2022-04-15 14:19:24 +0200

    BitmapaExt Construct and Destroy

--- Morfologia.cpp
+++ Morfologia.cpp
@@ -5,21 +5,36 @@
 
 #include "Morfologia.h"
 
-
 /** Klasa reprezentujaca bitmapy 3D.*/
 class BitmapaExt : public Bitmapa{
    /*--------------Pola-----------------*/
    /**Kazdy bit mapy moze przyjmowac jeden z kolorow:
-    * Bialy lub Czarny. */
-   enum Color{ WHITE, BLACK };
+    * Bialy lub Czarny.
+    * */
+   enum Color{ WHITE = 0, BLACK = 1 };
 
    /**Pola zawierajace rozmiar bitmapy*/
    unsigned int rangeX,rangeY,rangeZ;
 
    /**Pole reperzetujace Bitmape.
-    * Dostep: bitmap[x][y][z]*/
-   Color** bitmap;
+    * */
+   Color* bitmap;
 
    /*--------------Metody----------------*/
+public:
+   /**Konstruktor.
+    * Przyjmuje rozmar x,y,z, jako parametry.
+    * Tworzy biala bitmape.
+    * */
+    BitmapaExt(unsigned int x, unsigned int y, unsigned int z): rangeX{x},rangeY{y},rangeZ{z}{
+        bitmap = new Color[x*y*z]; // Tworzy tablice bitow o rozmiarze x*y*z.
+        for(auto i = 0; i<x*y*z; ++i )
+            bitmap[i] = WHITE;
+    }
+
+    /**Destruktor*/
+    ~BitmapaExt(){ delete[] bitmap; }
+
+
 
 };
