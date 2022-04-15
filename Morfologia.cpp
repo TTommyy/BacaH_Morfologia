733e8328ba87206d2d3986dfe20648a46aed6d76
Tommy <tomekkoczar@gmail.com>
2022-04-15 14:52:21 +0200

    BitmapaExt Bool Modification and Accessors

--- Morfologia.cpp
+++ Morfologia.cpp
@@ -8,17 +8,14 @@
 /** Klasa reprezentujaca bitmapy 3D.*/
 class BitmapaExt : public Bitmapa{
    /*--------------Pola-----------------*/
-   /**Kazdy bit mapy moze przyjmowac jeden z kolorow:
-    * Bialy lub Czarny.
-    * */
-   enum Color{ WHITE = 0, BLACK = 1 };
 
    /**Pola zawierajace rozmiar bitmapy*/
-   unsigned int rangeX,rangeY,rangeZ;
+   unsigned rangeX,rangeY,rangeZ;
 
    /**Pole reperzetujace Bitmape.
-    * */
-   Color* bitmap;
+    * Kazdy bit moze byc bialy = false = 0.
+    * Badz czarny = true != 0*/
+   bool* bitmap;
 
    /*--------------Metody----------------*/
 public:
@@ -26,15 +23,37 @@ public:
     * Przyjmuje rozmar x,y,z, jako parametry.
     * Tworzy biala bitmape.
     * */
-    BitmapaExt(unsigned int x, unsigned int y, unsigned int z): rangeX{x},rangeY{y},rangeZ{z}{
-        bitmap = new Color[x*y*z]; // Tworzy tablice bitow o rozmiarze x*y*z.
+    BitmapaExt(unsigned x, unsigned y, unsigned z): rangeX{x},rangeY{y},rangeZ{z}{
+        bitmap = new bool[x*y*z]; // Tworzy tablice bitow o rozmiarze x*y*z.
         for(auto i = 0; i<x*y*z; ++i )
-            bitmap[i] = WHITE;
+            bitmap[i] = false;//Ustawiamy kolor kazdego na bialy
     }
 
     /**Destruktor*/
-    ~BitmapaExt(){ delete[] bitmap; }
+    ~BitmapaExt() override{ delete[] bitmap; }
 
+    /**Getery dla rozmiaru*/
+    unsigned sx() const override{
+        return rangeX;
+    }
+
+    unsigned sy() const override{
+        return rangeY;
+    }
+
+    unsigned sz() const override{
+        return rangeZ;
+    }
+
+    /*Akcesory*/
+    /**Akcesor do referencji*/
+    bool& operator()(unsigned x, unsigned y, unsigned z) override{
+        return bitmap[x*(rangeY*rangeZ)+(y*rangeZ)+z];
+    }
+    /**Akcesor do kopi*/
+    bool operator()(unsigned x, unsigned y, unsigned z)const override{
+        return bitmap[x*(rangeY*rangeZ)+(y*rangeZ)+z];
+    }
 
 
 };
