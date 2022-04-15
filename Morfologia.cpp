d16c679aaf776a06df51d2d89a6ced1d1d7c502b
Tommy <tomekkoczar@gmail.com>
2022-04-15 13:44:08 +0200

    BitmapaExt fields

--- Morfologia.cpp
+++ Morfologia.cpp
@@ -1,5 +1,25 @@
-//
-// Created by tomek on 15.04.2022.
-//
+// Tomasz Koczar
+// 15.04.2022AD
+// Zadanie H
+// Kurs "Programowanie 2"
 
 #include "Morfologia.h"
+
+
+/** Klasa reprezentujaca bitmapy 3D.*/
+class BitmapaExt : public Bitmapa{
+   /*--------------Pola-----------------*/
+   /**Kazdy bit mapy moze przyjmowac jeden z kolorow:
+    * Bialy lub Czarny. */
+   enum Color{ WHITE, BLACK };
+
+   /**Pola zawierajace rozmiar bitmapy*/
+   unsigned int rangeX,rangeY,rangeZ;
+
+   /**Pole reperzetujace Bitmape.
+    * Dostep: bitmap[x][y][z]*/
+   Color** bitmap;
+
+   /*--------------Metody----------------*/
+
+};
